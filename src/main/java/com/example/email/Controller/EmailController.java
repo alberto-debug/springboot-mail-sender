d61caa82765a38ed049071493299bf2c1b28e201
Alberto <albertojunior848423803@gmail.com>
2025-03-24 13:32:51 +0300

    changes

--- src/main/java/com/example/email/Controller/EmailController.java
+++ src/main/java/com/example/email/Controller/EmailController.java
@@ -1,8 +1,11 @@
 package com.example.email.Controller;
 
 
+import com.example.email.model.EmailDetails;
 import com.example.email.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,20 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    @PostMapping("/sendMail")
+    public String sendMail(@RequestBody EmailDetails details){
+        String status = emailService.sendSimpleMail(details);
+        return status;
+    }
 
+    // Sending email with attachment
+    @PostMapping("/sendMailWithAttachment")
+    public String sendMailWithAttachment(
+            @RequestBody EmailDetails details)
+    {
+        String status
+                = emailService.sendMailWithAttachment(details);
+
+        return status;
+    }
 }
