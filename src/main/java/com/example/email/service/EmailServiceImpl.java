ed7b137b3372071212d53fe6778c42a2f256be81
Alberto <albertojunior848423803@gmail.com>
2025-03-24 13:28:12 +0300

    changes

--- src/main/java/com/example/email/service/EmailServiceImpl.java
+++ src/main/java/com/example/email/service/EmailServiceImpl.java
@@ -1,4 +1,28 @@
 package com.example.email.service;
 
-public class EmailServiceImpl {
+import com.example.email.model.EmailDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+
+    @Override
+    public String sendSimpleMail(EmailDetails details) {
+        return "";
+    }
+
+    @Override
+    public String sendMailWithAttachment(EmailDetails details) {
+        return "";
+    }
 }
